bb49327ec6c5cb3e80166d409f59f69ad8256470
Kohichi Aoki <drikin@gmail.com>
2013-03-30 09:55:16 -0700

    clean default setting at nginx install

--- config/sunzi/roles/web.sh
+++ config/sunzi/roles/web.sh
@@ -3,6 +3,12 @@
 aptitude -y install nginx       # Nginx
 # aptitude -y install apache2   # Apache
 
+if sunzi.install "nginx"; then
+    pushd /etc/nginx/sites-enabled
+    rm *
+    pupd
+fi
+
 if sunzi.installed "nginx"; then
   /usr/sbin/nginx -s quit
   /usr/sbin/nginx
