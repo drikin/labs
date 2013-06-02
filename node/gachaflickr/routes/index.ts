53d5e209e7c5e96f4c88a9230d76a5931a5012ed
Kohichi Aoki <drikin@gmail.com>
2013-06-03 00:55:01 +0900

    fixed undef issue

--- node/gachaflickr/routes/index.ts
+++ node/gachaflickr/routes/index.ts
@@ -2,8 +2,10 @@ import express = module("express")
 
 export function index(req, res){
   var user = req.user;
-  if (user&&user.displayName === undefined) {
-    user.displayName = 'GUEST';
+  if (user&&user.displayName !== undefined) {
+    var name = user.displayName;
+  } else {
+    var name = 'GUEST';
   }
-  res.render('index', {title: user.displayName, user: JSON.stringify(user)})
+  res.render('index', {title: name, user: JSON.stringify(user)})
 };
